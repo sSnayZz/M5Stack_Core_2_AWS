e3ba58efb01d9dfe72e1908ec849afca9c0c2831
p2205621 <nathan.alves@etu.univ-lyon1.fr>
2026-09-03 09:23:26 +0200

    serial monitoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
-#include <Arduino.h>
 #include <M5Unified.h>  
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  M5.begin();
-
+  
+  m5::M5Unified::config_t cfg = M5.config();
+  cfg.serial_baudrate = 115200;
+  M5.begin(cfg);
   M5.Lcd.setTextSize(2);
   M5.Lcd.setTextColor(TFT_YELLOW);
   M5.Lcd.setCursor(80,0);
 
   M5.Lcd.println("Hello World !");
+  Serial.println("Hello World !");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.printf("%06lu : Loop !\n", millis());
+  delay(2000);
 }
 
 // put function definitions here:
