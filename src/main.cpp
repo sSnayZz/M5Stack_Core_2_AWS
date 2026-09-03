4485e0e9fc25312e170c867f7ad8e2e4166c33bc
p2205621 <nathan.alves@etu.univ-lyon1.fr>
2026-09-03 08:44:58 +0200

    Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
+#include <M5Unified.h>  
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  M5.begin();
+
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextColor(TFT_YELLOW);
+  M5.Lcd.setCursor(80,0);
+
+  M5.Lcd.println("Hello World !");
 }
 
 void loop() {
